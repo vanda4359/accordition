455b47e5d3c217cb9da1eaaa4262f396d5240956
Van Dang Anh <vandait996@gmail.com>
2023-01-30 15:43:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Faq from "./components/faq/Faq";
+
+function App() {
+  return (
+    <div>
+      <Faq/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/faq/Data.js
@@ -0,0 +1,17 @@
+export const questions = [
+    {
+        id: 1,
+        title: "Can I learn ReactJS?",
+        answer: "Yes, you can learn ReactJS at ReactJS.org",
+    },
+    {
+        id: 1,
+        title: "Can I learn JavaScript?",
+        answer: "Yes, you can learn JavaScript at www.w3schools.com",
+    },
+    {
+        id: 1,
+        title: "Can I learn HTML, CSS?",
+        answer: "Yes, you can learn HTML, CSS at www.w3schools.com",
+    }
+]
